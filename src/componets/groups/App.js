5bfb0541577c6259b6b6907aae5f6c443ad99a72
SvyTo <sv_borisov03@mail.ru>
2022-07-02 00:52:09 +0400

    1. First Sommit
    @SvyTo

---
+++ src/componets/groups/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import Counter from "../tags/Counter";
+import ClassCounter from "../tags/ClassCounter";
+
+function App() {
+    const [value, setValue] = useState('')
+
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default App;
